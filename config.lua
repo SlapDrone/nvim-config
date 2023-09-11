d3990a848f01198dd3fafe3733616f55e8459a0a
Liam Moore <lmoore90@gmail.com>
2023-09-11 20:25:28 +0200

    lvim

---
+++ config.lua
@@ -0,0 +1,485 @@
+reload("liam.options")
+-- markdown editing
+require("lvim.lsp.manager").setup("marksman")
+-- install plugins
+lvim.plugins = {
+  "ChristianChiarulli/swenv.nvim",
+  "stevearc/dressing.nvim",
+  "mfussenegger/nvim-dap-python",
+  "nvim-neotest/neotest",
+  "nvim-neotest/neotest-python",
+  "simrat39/rust-tools.nvim",
+  "AckslD/nvim-trevJ.lua",
+  -- {
+  --   "iamcco/markdown-preview.nvim",
+  --   build = "cd app && npm install",
+  --   ft = "markdown",
+  --   config = function()
+  --     vim.g.mkdp_auto_start = 1
+  --   end,
+  -- },
+  {"ellisonleao/glow.nvim", config = function() require("glow").setup() end},
+  {"AckslD/nvim-trevJ.lua", config = function() require("trevj").setup() end},
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim"
+    },
+    config = function()
+      require("chatgpt").setup({
+      api_key_cmd = nil,
+      yank_register = "+",
+      edit_with_instructions = {
+        diff = false,
+        keymaps = {
+          close = "<C-c>",
+          accept = "<C-y>",
+          toggle_diff = "<C-d>",
+          toggle_settings = "<C-o>",
+          cycle_windows = "<Tab>",
+          use_output_as_input = "<C-i>",
+        },
+      },
+      chat = {
+        loading_text = "Loading, please wait ...",
+        question_sign = "",
+        answer_sign = "ﮧ",
+        max_line_length = 120,
+        sessions_window = {
+          border = {
+            style = "rounded",
+            text = {
+              top = " Sessions ",
+            },
+          },
+          win_options = {
+            winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+          },
+        },
+        keymaps = {
+          close = { "<C-c>" },
+          yank_last = "<C-y>",
+          yank_last_code = "<C-k>",
+          scroll_up = "<C-u>",
+          scroll_down = "<C-d>",
+          new_session = "<C-n>",
+          cycle_windows = "<Tab>",
+          cycle_modes = "<C-f>",
+          select_session = "<Space>",
+          rename_session = "r",
+          delete_session = "d",
+          draft_message = "<C-d>",
+          toggle_settings = "<C-o>",
+          toggle_message_role = "<C-r>",
+          toggle_system_role_open = "<C-s>",
+          stop_generating = "<C-x>",
+        },
+      },
+      popup_layout = {
+        default = "center",
+        center = {
+          width = "80%",
+          height = "80%",
+        },
+        right = {
+          width = "30%",
+          width_settings_open = "50%",
+        },
+      },
+      popup_window = {
+        border = {
+          highlight = "FloatBorder",
+          style = "rounded",
+          text = {
+            top = " ChatGPT ",
+          },
+        },
+        win_options = {
+          wrap = true,
+          linebreak = true,
+          foldcolumn = "1",
+          winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+        },
+        buf_options = {
+          filetype = "markdown",
+        },
+      },
+      system_window = {
+        border = {
+          highlight = "FloatBorder",
+          style = "rounded",
+          text = {
+            top = " SYSTEM ",
+          },
+        },
+        win_options = {
+          wrap = true,
+          linebreak = true,
+          foldcolumn = "2",
+          winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+        },
+      },
+      popup_input = {
+        prompt = "  ",
+        border = {
+          highlight = "FloatBorder",
+          style = "rounded",
+          text = {
+            top_align = "center",
+            top = " Prompt ",
+          },
+        },
+        win_options = {
+          winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+        },
+        submit = "<C-Enter>",
+        submit_n = "<Enter>",
+        max_visible_lines = 20
+      },
+      settings_window = {
+        border = {
+          style = "rounded",
+          text = {
+            top = " Settings ",
+          },
+        },
+        win_options = {
+          winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+        },
+      },
+      openai_params = {
+        model = "gpt-4",
+        frequency_penalty = 0,
+        presence_penalty = 0,
+        max_tokens = nil,
+        temperature = 0.2,
+        top_p = 1,
+        n = 1,
+      },
+      openai_edit_params = {
+        model = "gpt-4",
+        temperature = 0,
+        top_p = 1,
+        n = 1,
+      },
+      actions_paths = {},
+      show_quickfixes_cmd = "Trouble quickfix",
+      predefined_chat_gpt_prompts = "https://raw.githubusercontent.com/f/awesome-chatgpt-prompts/main/prompts.csv",
+    })
+    end,
+  },
+  {
+    "saecki/crates.nvim",
+    version = "v0.3.0",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("crates").setup {
+        null_ls = {
+          enabled = true,
+          name = "crates.nvim",
+        },
+        popup = {
+          border = "rounded",
+        },
+      }
+    end,
+  },
+  {
+    "j-hui/fidget.nvim",
+    version = 'legacy',
+    config = function()
+      require("fidget").setup()
+    end,
+  },
+  {
+    "p00f/clangd_extensions.nvim",
+  },
+}
+
+-- automatically install python syntax highlighting
+lvim.builtin.treesitter.ensure_installed = {
+  "python",
+  "lua",
+  "rust",
+  "toml",
+  "cpp",
+  "c",
+}
+
+-- setup formatting
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup { { name = "black" }, }
+lvim.format_on_save.enabled = true
+lvim.format_on_save.pattern = { "*.py" }
+
+-- setup linting
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup { { command = "flake8", filetypes = { "python" } } }
+
+-- setup debug adapter
+lvim.builtin.dap.active = true
+local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
+pcall(function()
+  require("dap-python").setup(mason_path .. "packages/debugpy/venv/bin/python")
+end)
+
+-- setup testing
+require("neotest").setup({
+  adapters = {
+    require("neotest-python")({
+      -- Extra arguments for nvim-dap configuration
+      -- See https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for values
+      dap = {
+        justMyCode = false,
+        console = "integratedTerminal",
+      },
+      args = { "--log-level", "DEBUG", "--quiet" },
+      runner = "pytest",
+    })
+  }
+})
+
+lvim.builtin.which_key.mappings["dm"] = { "<cmd>lua require('neotest').run.run()<cr>",
+  "Test Method" }
+lvim.builtin.which_key.mappings["dM"] = { "<cmd>lua require('neotest').run.run({strategy = 'dap'})<cr>",
+  "Test Method DAP" }
+lvim.builtin.which_key.mappings["df"] = {
+  "<cmd>lua require('neotest').run.run({vim.fn.expand('%')})<cr>", "Test Class" }
+lvim.builtin.which_key.mappings["dF"] = {
+  "<cmd>lua require('neotest').run.run({vim.fn.expand('%'), strategy = 'dap'})<cr>", "Test Class DAP" }
+lvim.builtin.which_key.mappings["dS"] = { "<cmd>lua require('neotest').summary.toggle()<cr>", "Test Summary" }
+
+-- binding for switching
+lvim.builtin.which_key.mappings["C"] = {
+  name = "Python",
+  c = { "<cmd>lua require('swenv.api').pick_venv()<cr>", "Choose Env" },
+}
+
+-- Rust configuration
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "rust_analyzer" })
+
+local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
+
+local codelldb_path = mason_path .. "bin/codelldb"
+local liblldb_path = mason_path .. "packages/codelldb/extension/lldb/lib/liblldb"
+local this_os = vim.loop.os_uname().sysname
+
+-- The path in windows is different
+if this_os:find "Windows" then
+  codelldb_path = mason_path .. "packages\\codelldb\\extension\\adapter\\codelldb.exe"
+  liblldb_path = mason_path .. "packages\\codelldb\\extension\\lldb\\bin\\liblldb.dll"
+else
+  -- The liblldb extension is .so for linux and .dylib for macOS
+  liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
+end
+
+pcall(function()
+  require("rust-tools").setup {
+    tools = {
+      executor = require("rust-tools/executors").termopen, -- can be quickfix or termopen
+      reload_workspace_from_cargo_toml = true,
+      runnables = {
+        use_telescope = true,
+      },
+      inlay_hints = {
+        auto = true,
+        only_current_line = false,
+        show_parameter_hints = false,
+        parameter_hints_prefix = "<-",
+        other_hints_prefix = "=>",
+        max_len_align = false,
+        max_len_align_padding = 1,
+        right_align = false,
+        right_align_padding = 7,
+        highlight = "Comment",
+      },
+      hover_actions = {
+        border = "rounded",
+      },
+      on_initialized = function()
+        vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter", "CursorHold", "InsertLeave" }, {
+          pattern = { "*.rs" },
+          callback = function()
+            local _, _ = pcall(vim.lsp.codelens.refresh)
+          end,
+        })
+      end,
+    },
+    dap = {
+      -- adapter= codelldb_adapter,
+      adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
+    },
+    server = {
+      on_attach = function(client, bufnr)
+        require("lvim.lsp").common_on_attach(client, bufnr)
+        local rt = require "rust-tools"
+        vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
+      end,
+
+      capabilities = require("lvim.lsp").common_capabilities(),
+      settings = {
+        ["rust-analyzer"] = {
+          lens = {
+            enable = true,
+          },
+          checkOnSave = {
+            enable = true,
+            command = "clippy",
+          },
+        },
+      },
+    },
+  }
+end)
+
+lvim.builtin.dap.on_config_done = function(dap)
+  dap.adapters.codelldb = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path)
+  dap.configurations.rust = {
+    {
+      name = "Launch file",
+      type = "codelldb",
+      request = "launch",
+      program = function()
+        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+      end,
+      cwd = "${workspaceFolder}",
+      stopOnEntry = false,
+    },
+  }
+end
+
+vim.api.nvim_set_keymap("n", "<m-d>", "<cmd>RustOpenExternalDocs<Cr>", { noremap = true, silent = true })
+
+lvim.builtin.which_key.mappings["C"] = {
+  name = "Rust",
+  r = { "<cmd>RustRunnables<Cr>", "Runnables" },
+  t = { "<cmd>lua _CARGO_TEST()<cr>", "Cargo Test" },
+  m = { "<cmd>RustExpandMacro<Cr>", "Expand Macro" },
+  c = { "<cmd>RustOpenCargo<Cr>", "Open Cargo" },
+  p = { "<cmd>RustParentModule<Cr>", "Parent Module" },
+  d = { "<cmd>RustDebuggables<Cr>", "Debuggables" },
+  v = { "<cmd>RustViewCrateGraph<Cr>", "View Crate Graph" },
+  R = {
+    "<cmd>lua require('rust-tools/workspace_refresh')._reload_workspace_from_cargo_toml()<Cr>",
+    "Reload Workspace",
+  },
+  o = { "<cmd>RustOpenExternalDocs<Cr>", "Open External Docs" },
+  y = { "<cmd>lua require'crates'.open_repository()<cr>", "[crates] open repository" },
+  P = { "<cmd>lua require'crates'.show_popup()<cr>", "[crates] show popup" },
+  i = { "<cmd>lua require'crates'.show_crate_popup()<cr>", "[crates] show info" },
+  f = { "<cmd>lua require'crates'.show_features_popup()<cr>", "[crates] show features" },
+  D = { "<cmd>lua require'crates'.show_dependencies_popup()<cr>", "[crates] show dependencies" },
+}
+
+-- C/C++ configuration
+lvim.format_on_save = false
+-- lvim.lsp.diagnostics.virtual_text = true
+vim.diagnostic.config({ virtual_text = true })
+
+lvim.builtin.treesitter.highlight.enable = true
+
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "clangd" })
+
+-- some settings can only passed as commandline flags, see `clangd --help`
+local clangd_flags = {
+  "--background-index",
+  "--fallback-style=Google",
+  "--all-scopes-completion",
+  "--clang-tidy",
+  "--log=error",
+  "--suggest-missing-includes",
+  "--cross-file-rename",
+  "--completion-style=detailed",
+  "--pch-storage=memory", -- could also be disk
+  "--folding-ranges",
+  "--enable-config", -- clangd 11+ supports reading from .clangd configuration file
+  "--offset-encoding=utf-16", --temporary fix for null-ls
+  -- "--limit-references=1000",
+  -- "--limit-resutls=1000",
+  -- "--malloc-trim",
+  -- "--clang-tidy-checks=-*,llvm-*,clang-analyzer-*,modernize-*,-modernize-use-trailing-return-type",
+  -- "--header-insertion=never",
+  -- "--query-driver=<list-of-white-listed-complers>"
+}
+
+local provider = "clangd"
+
+local custom_on_attach = function(client, bufnr)
+  require("lvim.lsp").common_on_attach(client, bufnr)
+
+  local opts = { noremap = true, silent = true, buffer = bufnr }
+  vim.keymap.set("n", "<leader>lh", "<cmd>ClangdSwitchSourceHeader<cr>", opts)
+  vim.keymap.set("x", "<leader>lA", "<cmd>ClangdAST<cr>", opts)
+  vim.keymap.set("n", "<leader>lH", "<cmd>ClangdTypeHierarchy<cr>", opts)
+  vim.keymap.set("n", "<leader>lt", "<cmd>ClangdSymbolInfo<cr>", opts)
+  vim.keymap.set("n", "<leader>lm", "<cmd>ClangdMemoryUsage<cr>", opts)
+
+  require("clangd_extensions.inlay_hints").setup_autocmd()
+  require("clangd_extensions.inlay_hints").set_inlay_hints()
+end
+
+local status_ok, project_config = pcall(require, "rhel.clangd_wrl")
+if status_ok then
+  clangd_flags = vim.tbl_deep_extend("keep", project_config, clangd_flags)
+end
+
+local custom_on_init = function(client, bufnr)
+  require("lvim.lsp").common_on_init(client, bufnr)
+  require("clangd_extensions.config").setup {}
+  require("clangd_extensions.ast").init()
+  vim.cmd [[
+  command ClangdToggleInlayHints lua require('clangd_extensions.inlay_hints').toggle_inlay_hints()
+  command -range ClangdAST lua require('clangd_extensions.ast').display_ast(<line1>, <line2>)
+  command ClangdTypeHierarchy lua require('clangd_extensions.type_hierarchy').show_hierarchy()
+  command ClangdSymbolInfo lua require('clangd_extensions.symbol_info').show_symbol_info()
+  command -nargs=? -complete=customlist,s:memuse_compl ClangdMemoryUsage lua require('clangd_extensions.memory_usage').show_memory_usage('<args>' == 'expand_preamble')
+  ]]
+end
+
+local opts = {
+  cmd = { provider, unpack(clangd_flags) },
+  on_attach = custom_on_attach,
+  on_init = custom_on_init,
+}
+
+require("lvim.lsp.manager").setup("clangd", opts)
+
+-- install codelldb with :MasonInstall codelldb
+-- configure nvim-dap (codelldb)
+lvim.builtin.dap.on_config_done = function(dap)
+  dap.adapters.codelldb = {
+    type = "server",
+    port = "${port}",
+    executable = {
+      -- provide the absolute path for `codelldb` command if not using the one installed using `mason.nvim`
+      command = "codelldb",
+      args = { "--port", "${port}" },
+
+      -- On windows you may have to uncomment this:
+      -- detached = false,
+    },
+  }
+
+  dap.configurations.cpp = {
+    {
+      name = "Launch file",
+      type = "codelldb",
+      request = "launch",
+      program = function()
+        local path
+        vim.ui.input({ prompt = "Path to executable: ", default = vim.loop.cwd() .. "/build/" }, function(input)
+          path = input
+        end)
+        vim.cmd [[redraw]]
+        return path
+      end,
+      cwd = "${workspaceFolder}",
+      stopOnEntry = false,
+    },
+  }
+
+  dap.configurations.c = dap.configurations.cpp
+end
---
+++ lua/liam/options.lua
@@ -0,0 +1,74 @@
+-- https://www.youtube.com/watch?v=BGleksgsiD8 "neovim to lunarvim justin"
+vim.opt.relativenumber = true
+vim.opt.timeoutlen = 333
+vim.opt.showcmd = true
+lvim.transparent_window = true
+lvim.builtin.treesitter.ensure_installed = true
+-- Keymaps
+local keymap = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+vim.g.maplocalleader = '\\'
+-- normal
+keymap('n', ';', ':', {noremap = true})
+keymap('n', ':', ';', {noremap = true})
+keymap('n', "<LocalLeader>b", ":BufferLinePick<CR>", opts)
+keymap('n', "<LocalLeader>w", ":w<CR>", opts)
+keymap('n', "<LocalLeader>c", ":BufferKill<cr>", opts)
+keymap('n', "<LocalLeader>s", ":%s///g", opts)
+keymap('n', "<LocalLeader>j", ":lua require('trevj').format_at_cursor()<CR>", opts)
+keymap("n", "<S-h>", ":bprev<CR>", opts)
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+
+keymap('n', '<Leader>o', 'o<Esc>0"_D', {noremap = true, silent = true})
+keymap('n', '<Leader>O', 'O<Esc>0"_D', {noremap = true, silent = true})
+
+pcall(function()
+  require("colorizer").setup(
+    { "css", "scss", "html", "lua", "json", "toml", "yaml", },
+    {
+      RGB = true,
+      RRGGBB = true,
+      RRGGBBAA = true,
+      rgb_fn = true,
+      hsl_fn = true,
+      css = true,
+      css_fn = true,
+    }
+  )
+end)
+
+pcall(function()
+  require("Comment").setup()
+end)
+
+pcall(function()
+  require("scope").setup({
+    restore_state = true,
+  })
+end)
+
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  { name = "yamlfmt", filetypes = { "yaml"}} -- also black
+}
+
+local which_key = lvim.builtin.which_key
+which_key.setup = {
+  plugins = {
+    marks = true,
+    registers = true,
+    spelling = {
+      enabled = true,
+      suggestions = 20
+    },
+  }
+}
+which_key.mappings["w"]  = {
+  name = "Window",
+  c = { "<cmd>close<CR>", "Close Window"},
+}
+which_key.mappings["t"] = {
+  name = "Tabs",
+  n = { "<cmd>tabnew<CR>", "New Tab" },
+  c = { "<cmd>tabclose<cr>", "Close Tab"},
+}
